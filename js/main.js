b960934e21d2bd0f3c3ffbb88140a4b282f99145
Omar Ali Sheikh <sheikhomar@gmail.com>
2017-09-26 10:08:15 +0200

    Initialise camera instance.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function() {
   
+  camera = new JpegCamera("#camera", options).ready(function(info) {
+    // camera is ready
+    console.log("Camera resolution: " + info.video_width + "x" + info.video_height);
+
+  });
+
+  //$("#take_snapshots").click(function() {take_snapshots(3);});
+  $("#show_stream").click(function() {
+
+
+  });
 })
