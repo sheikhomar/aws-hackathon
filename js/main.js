15730293ffc193765fd192a1e2eebccc4ebd730f
Omar Ali Sheikh <sheikhomar@gmail.com>
2017-09-26 10:11:21 +0200

    Ops, forgot to create variable.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(function() {
   
+  var camera;
+
+
+
+  var options = {
+    shutter_ogg_url: "jpeg_camera/shutter.ogg",
+    shutter_mp3_url: "jpeg_camera/shutter.mp3",
+    swf_url: "jpeg_camera/jpeg_camera.swf"
+  };
+
   camera = new JpegCamera("#camera", options).ready(function(info) {
     // camera is ready
     console.log("Camera resolution: " + info.video_width + "x" + info.video_height);
